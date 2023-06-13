2cb2db70938765722286311f3252bf7728994894
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-06-12 19:20:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./gen.js');
-const fileName = "./logo.svg";
+const fileName = "./SVG IMG.png";
 const setShape = require('./SetShape.js')
 
 function createLogo(response) {
     const svg = setShape(response);
-    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+    fs.writeFile(fileName, svg, ()=> console.log('Generated SVG IMG.png'));
 }
 
 function init() {
