c7d63915d505be63ac2d1c1d3a0439278a283768
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-06-12 19:16:16 -0700

    Update SetShape.js

--- SetShape.js
+++ SetShape.js
@@ -1,1 +1,24 @@
 
+const Circle = require('./ShapeC.js');
+const Square = require('./ShapeS.js');
+const Triangle = require('./ShapeT.js');
+
+function setShape(response) {
+
+    if (response.shape === 'Circle') {
+        let userShape = new Circle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Square') {
+        let userShape = new Square (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Triangle') {
+        let userShape = new Triangle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+};
+
+module.exports = setShape;
